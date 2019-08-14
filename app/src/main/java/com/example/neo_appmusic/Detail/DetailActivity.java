06bc0b680e08912b13fe2c4b0e410ff4bc8b0528
Sontitxxxx <sonkoii1995@gmail.com>
2019-08-14 11:43:15 +0700

    Initial commit

---
+++ app/src/main/java/com/example/neo_appmusic/Detail/DetailActivity.java
@@ -0,0 +1,16 @@
+package com.example.neo_appmusic.Detail;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.neo_appmusic.R;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
